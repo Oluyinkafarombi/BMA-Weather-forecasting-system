8aa7724e983529c3ef9f2a3efe290e65ed7d4b19
Oluyinkafarombi <oluyinkafarombi@gmail.com>
2022-04-28 10:48:57 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const timeE1 = document.getElementById('time');
+const dateE1 = document.getElementById('date');
+const currentWeatherItemsE1 = document.getElementById('current-weather-items');
+const timezoneE1 = document.getElementById('time-zone');
+const countryE1 = document.getElementById('country');
+const weatherForecastE1 = document.getElementById('weather-forecast');
+const currenttempE1 = document.getElementById('current-temp');
+
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+    const minutes = time.getMinutes();
+    const ampm = hour >=12 ? 'PM' : 'AM'
+
+    timeE1.innerHTML = hoursIn12HrFormat + ':' + minutes+'' + `<span id="am-pm">${ampm}</span>`
+
+    dateE1.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
+
+}, 1000);
